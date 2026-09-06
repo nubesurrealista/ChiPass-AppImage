38f5ed15b297dc85bd01017904b0f415e4922820
Nube 云 <136946098+nubesurrealista@users.noreply.github.com>
2026-09-06 15:14:33 -0400

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,8 +15,8 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/local/share/icons/hicolor/256x256/apps/chipass.png
-export DESKTOP=/usr/local/share/applications/org.chipass.ChiPass.desktop
+export ICON=/usr/share/icons/hicolor/256x256/apps/chipass.png
+export DESKTOP=/usr/share/applications/org.chipass.ChiPass.desktop
 export ALWAYS_SOFTWARE=1
 
 # On Arch Linux qt5-wayland also adds the server-side plugins
@@ -26,8 +26,10 @@ rm -rf /usr/lib/qt/plugins/wayland-graphics-integration-server 2>/dev/null || tr
 
 # Deploy dependencies
 quick-sharun \
-	/usr/local/bin/chipass* \
-	/usr/lib/libpcsclite*.so*
+	/usr/bin/chipass* \
+	/usr/lib/chipass \
+	/usr/lib/keepassxc \
+	/usr/lib/libpcsclite_real.so*
 
 # Additional changes can be done in between here
 
