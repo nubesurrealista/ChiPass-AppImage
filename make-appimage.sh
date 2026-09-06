beff5e8badcec7ec1bd4a6f5a287c0cb2d5cd783
Nube 云 <136946098+nubesurrealista@users.noreply.github.com>
2026-09-06 08:05:51 -0400

    test2

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,24 +3,29 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q keepassxc | awk '{print $2; exit}') # example command to get version of application here
+
+VERSION=$(build/src/chipass --version 2>/dev/null | awk '{print $2}' || echo "latest")
+
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/apps/keepassxc.png
-export DESKTOP=/usr/share/applications/org.keepassxc.KeePassXC.desktop
+export ICON=/usr/share/icons/hicolor/256x256/apps/chipass.png
+export DESKTOP=/usr/share/applications/org.chipass.ChiPass.desktop
 export ALWAYS_SOFTWARE=1
 
 # on archlinux qt5-wayland also adds the server side plugins
 # remove them so that they do not get deployed
-rm -rf /usr/lib/qt/plugins/wayland-graphics-integration-server
+rm -rf /usr/lib/qt6/plugins/wayland-graphics-integration-server 2>/dev/null || true
+rm -rf /usr/lib/qt/plugins/wayland-graphics-integration-server 2>/dev/null || true
+
+DESTDIR=/ cmake --install build
 
 # Deploy dependencies
 quick-sharun \
-	/usr/bin/keepassxc* \
-	/usr/lib/keepassxc  \
-	/usr/lib/libpcsclite_real.so*
+	/usr/bin/chipass* \
+	/usr/lib/chipass* \
+	/usr/lib/libpcsclite*.so*
 
 # Additional changes can be done in between here
 
